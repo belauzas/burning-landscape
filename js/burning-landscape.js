48f3e11cf7043d733f9ab69fccea156b5ff11199
Rimantas <belauzas@yahoo.com>
2019-08-10 01:59:27 +0300

    Player movement

--- js/burning-landscape.js
+++ js/burning-landscape.js
@@ -17,6 +17,14 @@ class burningLandscape  {
             cellsY: 0,
             cellCount: 0
         }
+        this.player = {
+            unitCount: 24,
+            selectedUnit: 1,
+            position: {
+                x: 0,
+                y: 0
+            }
+        }
         this.level = {
             selected: 0,
             list: [
@@ -48,6 +56,7 @@ class burningLandscape  {
         this.field = this.target.querySelector('#field');
         this.background = this.field.querySelector('.background');
         this.objects = this.field.querySelector('.objects');
+        this.unit = this.target.querySelector('.player > .unit');
 
         // reset game settings
         this.screen.width = parseInt(getComputedStyle(this.field).width);
@@ -70,6 +79,8 @@ class burningLandscape  {
         }
 
         this.obstacles();
+        // reset player chracter
+        this.playerChracterReset();
 
         // reset cells
         this.background.innerHTML = this.HTML.background.tile
@@ -77,7 +88,7 @@ class burningLandscape  {
         let html = '';
         this.map.map( row => 
             row.map( column => {
-                if ( column > 0 && column <= this.HTML.objects.trees ) {
+                if ( column >= 0 && column <= this.HTML.objects.trees ) {
                     html += `<div class="cell trees-${column}"></div>`;
                 }
                 if ( column === 'r' ) {
@@ -86,7 +97,9 @@ class burningLandscape  {
             })
         );
         this.objects.innerHTML = html;
-        
+
+        // DOM events
+        window.addEventListener('keyup', this.moveUnit);
     }
 
     obstacles = () => {
@@ -105,6 +118,57 @@ class burningLandscape  {
             const y = (cellID - x) / w;
             this.map[y][x] = 'r';
         });
+    }
+
+    playerChracterReset = () => {
+        this.player.position.x = Math.floor( this.screen.cellsX / 2 );
+        this.player.position.y = this.screen.cellsY - 1;
+
+        this.unit.style.top = this.player.position.y * this.cellSize + 'px';
+        this.unit.style.left = this.player.position.x * this.cellSize + 'px';
+
+        // clear space around player
+        for ( let x=-1; x<=1; x++ ) {
+            for ( let y=-1; y<=1; y++ ) {
+                if ( this.player.position.x + x >= 0 &&
+                     this.player.position.x + x < this.screen.cellsX &&
+                     this.player.position.y + y >= 0 &&
+                     this.player.position.y + y < this.screen.cellsY
+                    ) {
+                    this.map[ this.player.position.y + y ][ this.player.position.x + x ] = 0;
+                }
+            }
+        }
+    }
+
+    moveUnit = ( event ) => {
+        let move = {x:0, y:0};
+        switch (event.keyCode) {
+            // up
+            case 87: move.y--; break;
+            // left
+            case 65: move.x--; break;
+            // down
+            case 83: move.y++; break;
+            // right
+            case 68: move.x++; break;
+            default: break;
+        }
+
+        // move unit only if it's not going to move over the obstacle or out of the field
+        const horizontal = this.player.position.x + move.x;
+        const vertical = this.player.position.y + move.y;
+        if ( horizontal >= 0 &&
+             horizontal < this.screen.cellsX &&
+             vertical >= 0 &&
+             vertical < this.screen.cellsY &&
+             this.map[ vertical ][ horizontal ] === 0 ) {
+            this.player.position.x += move.x;
+            this.player.position.y += move.y;
+
+            this.unit.style.top = this.player.position.y * this.cellSize + 'px';
+            this.unit.style.left = this.player.position.x * this.cellSize + 'px';
+        }
     }
 }
 
